4827e8caab2171eb0f8da7dc6bccc244495f094d
OksanaFedyukova <avseniya22@gmail.com>
2024-07-01 15:51:18 +0200

    added Switch

--- MODULO_1/HTML_CSS/UNIDAD_2_JS/JAVASCRIPT/010_Switch_Case/main.js
+++ MODULO_1/HTML_CSS/UNIDAD_2_JS/JAVASCRIPT/010_Switch_Case/main.js
@@ -1,19 +1,44 @@
+const month = parseInt(prompt('When is your Birthday ? Write number of Month'));
+const userMonth = document.querySelector('#month');
 
-let userSum;
-const suma = 17;
-const correctAnswer = document.querySelector('#success');
-
-const isHuman = ()=> {
-  userSum = parseInt(prompt('Write correct answer: 8 + 9 is...'));
-
-  if (isNaN(userSum)) {
-    alert('That"s is Not a Number.');
-    isHuman(); 
-  } if (userSum !== suma) {
-    alert('I am sorry ...you are WRONG TRY AGAIN!');
-    isHuman(); 
-  } 
-  correctAnswer.innerHTML = `GREAT!`; 
+switch (month) {
+  case 1:
+    userMonth.innerHTML = "January";
+    break;
+  case 2:
+    userMonth.innerHTML = "February";
+    break;
+  case 3:
+    userMonth.innerHTML = "March";
+    break;
+  case 4:
+    userMonth.innerHTML = "April";
+    break;
+  case 5:
+    userMonth.innerHTML = "May";
+    break;
+  case 6:
+    userMonth.innerHTML = "June";
+    break;
+  case 7:
+    userMonth.innerHTML = "July";
+    break;
+  case 8:
+    userMonth.innerHTML = "August";
+    break;
+  case 9:
+    userMonth.innerHTML = "September";
+    break;
+  case 10:
+    userMonth.innerHTML = "October";
+    break;
+  case 11:
+    userMonth.innerHTML = "November";
+    break;
+  case 12:
+    userMonth.innerHTML = "December";
+    break;
+  default:
+    userMonth.innerHTML = "Ooops this month doesn't exist";
+    break;
 }
-
-isHuman(); 
