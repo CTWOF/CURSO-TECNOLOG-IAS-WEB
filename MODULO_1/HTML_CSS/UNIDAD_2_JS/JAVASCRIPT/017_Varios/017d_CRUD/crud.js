a2b098d18beef4caa1483936872334ff1507440d
OksanaFedyukova <avseniya22@gmail.com>
2024-07-09 11:21:26 +0200

    add crud 017

--- MODULO_1/HTML_CSS/UNIDAD_2_JS/JAVASCRIPT/017_Varios/017d_CRUD/crud.js
+++ MODULO_1/HTML_CSS/UNIDAD_2_JS/JAVASCRIPT/017_Varios/017d_CRUD/crud.js
@@ -1,8 +1,23 @@
-/* (c) Ejercicio Añadir y eliminar de la lista: Lista de tareas
-Dentro de la carpeta creada ejercicios_varios crea una 
-carpeta llamada anadir_eliminar_lista
-Crea los archivos index.html, main.js y styles.css y 
-vincúlalos.
-En el archivo html crea un input para insertar nombres de tareas, un botón para agregarlos, y una lista donde se mostrarán los nombres con un aspa que al pulsarla se elimina ese elemento de la lista.
-Usa el css necesario para que la caja se parezca al wireframe */
+let tasks = [];
 
+
+document.getElementById('saveTask').addEventListener('click', () => {
+    let taskName = document.getElementById('taskName').value;
+    if (taskName) {
+        tasks.push(taskName);
+        let listTask = document.createElement('li');
+        listTask.textContent = taskName;
+
+        let deleteTask = document.createElement('button');
+        deleteTask.textContent = 'Delete';
+        deleteTask.id = 'deleteTask';
+        deleteTask.addEventListener('click', () => {
+            listTask.remove();
+            tasks = tasks.filter(task => task !== taskName);
+        });
+
+        listTask.appendChild(deleteTask);
+        document.getElementById('listTask').appendChild(listTask);
+        document.getElementById('taskName').value = '';
+    }
+});
