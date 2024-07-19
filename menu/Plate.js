e07c3e9b96d596e12f7fa65e2c1d7c498104c48d
OksanaFedyukova <avseniya22@gmail.com>
2024-07-19 10:17:58 +0200

    +

--- menu/Plate.js
+++
@@ -1,9 +0,0 @@
-class Plate {
-  constructor(id, title, description, price) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.price = price;
-  }
-}
-export default Plate;
--- menu/helpers.js
+++
@@ -1,8 +0,0 @@
-
-export const switchSection = (section) => {
-    const sections = ['plates-section', 'menu-section', 'ofert-section'];
-    sections.forEach(sec => {
-      document.getElementById(sec).style.display = sec === section ? 'block' : 'none';
-    });
-  };
-  
